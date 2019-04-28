05e50d4c4c0a8feb8fcf854279aa6d923281d858
jean-michel-gonet <jmgonet@yahoo.com>
2019-04-28 08:44:16 +0200

    Acquire the VCC level

--- p-module.X/main.c
+++ p-module.X/main.c
@@ -14,11 +14,48 @@
 
 #ifndef TEST
 
-void main(void) {
-    int n;
-    for (n = 0; n < 100; n++) {
-        n--;
+void initializeHardware() {
+    // Configure Fosc at 64Mhz
+    OSCCONbits.IRCF = 7;
+    OSCTUNEbits.PLLEN = 1;
+
+    // Activate A/D converters AN16 and AN17:
+    ANSELA = 0x00;
+    ANSELB = 0x00;
+    ANSELC = 0x00;
+    ANSELCbits.ANSC5 = 1; // RC5 (AN17)
+    ANSELCbits.ANSC4 = 1; // RC4 (AN16)
+    
+    ADCON2bits.ADFM = 0;   // Conversion result is left justified on ADRESH
+    ADCON2bits.ACQT = 0;   // No reason to wait after GO/DONE is set.
+    ADCON2bits.ADCS = 6;   // TAD of 1µS when Fosc is at 64MHz
+
+    ADCON0bits.CHS = 17;   // Let's acquire first the VCC value.
+    ADCON0bits.ADON = 1;   // Turn it on.
+    
+    IPR1bits.ADIP = 0;     // Low priority interrupt.
+    PIE1bits.ADIE = 1;     // Enable interrupts.
+    
+    // Enable interruptions:
+    RCONbits.IPEN = 1;     // Let's have high / low priority interruptions.
+    INTCONbits.GIEH = 1;   // Enable interrupts.
+    INTCONbits.GIEL = 1;   // Enable low priority interrupts.
+}
+
+void __interrupt(low_priority) lowPriorityInterrupts(void) {
+
+    // Just captured an AD conversion:
+    if (PIR1bits.ADIF) {
+        PIR1bits.ADIF = 0;
+        CCPR1L = ADRESH;
+        ADCON0bits.GO = 1;
     }
+}
+
+
+void main(void) {
+    initializeHardware();
+    ADCON0bits.GO = 1;
     while(1);
 }
 
